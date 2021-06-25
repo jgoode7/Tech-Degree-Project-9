f79dd37dd96f163030d0ab6bf037b34fefb09dc8
jgoode7 <78943773+jgoode7@users.noreply.github.com>
2021-06-24 19:41:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let closeBtn = document.querySelector('.close');
 
 modal.onclick = function() {
     modal.style.display = 'block';
-}
+};
 
 closeBtn.onclick = function() {
     modal.style.display = 'none';
-}
+};
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
     }
-}
+};
